78f8cdcaa16f64b50f4021007cdf2e3eaecf65d0
johan <johanirfan123@gmail.com>
2025-12-31 14:12:44 +0800

    add https

--- resources/views/visitor/index.blade.php
+++ resources/views/visitor/index.blade.php
@@ -31,7 +31,7 @@
                        class="flex-grow border-gray-300 rounded-l-xl px-5 py-3 text-gray-700 placeholder-gray-400 focus:ring-green-500 focus:border-green-500 transition duration-150 ease-in-out">
                 <button onclick="searchGrave()" 
                         class="bg-green-600 hover:bg-green-700 text-white font-semibold px-6 py-3 rounded-r-xl transition duration-300 ease-in-out flex items-center justify-center">
-                    <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                    <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                     {{ __('messages.search') }}
                 </button>
             </div>
